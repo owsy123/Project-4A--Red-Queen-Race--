677dc0060c242dc874d8a065250dacb0ea9898b6
owsy123 <owais.sohail3@gmail.com>
2020-07-18 18:14:38 +0500

    update work

---
+++ build/precache-manifest.36a00d7b5c7730a0accbc2404c455ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602a2e7563d08adde19e03c842627b4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3602ed5e79daefb1b3c",
+    "url": "/static/css/main.616317ec.chunk.css"
+  },
+  {
+    "revision": "9766491af015d55f4caa",
+    "url": "/static/js/2.f409b2cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f409b2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3602ed5e79daefb1b3c",
+    "url": "/static/js/main.0efb641d.chunk.js"
+  },
+  {
+    "revision": "6b6e76b9e3f36c351abb",
+    "url": "/static/js/runtime-main.fef72960.js"
+  }
+]);
